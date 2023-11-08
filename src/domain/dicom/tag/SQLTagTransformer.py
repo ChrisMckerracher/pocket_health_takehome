b8a8de7fe539be5d6d95acacc92a0536d68fae85
Christopher Mckerracher <chrislmckerracher@gmail.com>
2023-11-08 00:08:35 -0500

    in prog transformer

--- src/domain/dicom/tag/SQLTagTransformer.py
+++ src/domain/dicom/tag/SQLTagTransformer.py
@@ -1,5 +1,5 @@
 from operator import attrgetter
-from typing import List
+from typing import List, ForwardRef, Optional
 
 from src.db.dicom.dicom import DataSetItem
 from src.domain.dicom.tag.dicom_tag import DicomTag
@@ -7,7 +7,13 @@ from src.domain.dicom.tag.dicom_tag import DicomTag
 from itertools import groupby
 
 
+class DataSetTree:
+    id: str
+    children: Optional[List[ForwardRef("DataSetTree")]] = None
+
 def transform(dataset_items: List[DataSetItem]) -> DicomTag:
     for item in groupby(dataset_items, key=attrgetter("data_set_id")):
         pass
     return None
+
+
